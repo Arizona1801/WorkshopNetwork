45fe0af8fdabc0215fb26435444a1f8a176251b9
khlooe <c.moalic@lecolededesign.com>
2025-05-15 16:32:46 +0200

    Add SVG icon and CSS styles for game interface
    - Introduced a new SVG icon file for the web client.
    - Created a comprehensive CSS stylesheet for the game, defining styles for various elements including typography, layout, and animations.
    - Implemented responsive design features to ensure compatibility across different screen sizes.
    - Established a color scheme using CSS variables for easier theme management.

---
+++ webclient/scriptDecollage.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let timeLeft = 10;
+    const timerDisplay = document.getElementById('timer');
+
+    function updateTimerDisplay() {
+        timerDisplay.textContent = timeLeft;
+    }
+
+    function countdown() {
+        if (timeLeft >= 0) {
+            updateTimerDisplay();
+            if (timeLeft === 0) {
+                timerDisplay.textContent = "C'est parti!";
+                // Redirection vers un lien
+                window.location.href = "http://10.10.16.179/game.html"; // Remplacez par votre URL
+            } else {
+                timeLeft--; // Décrémente après la mise à jour de l'affichage
+                setTimeout(countdown, 1000); // Appelle countdown toutes les 1000ms (1 seconde)
+            }
+        }
+    }
+
+    updateTimerDisplay(); // Affiche le temps initial
+    countdown(); // Démarre le décompte
+});
